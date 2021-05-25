d0a142cf6420d4f2889e19fe9dd1ff9a1ea0a2fc
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-05-26 01:18:34 +0530

    welcome page created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
 @section('page-content')
 
 
-<section class="py-20">
-  <div class="bg-gradient-to-r from-green-400 to-blue-500 px-8 py-10">
+<section class="relative min-h-screen flex items-center">
+  <div class="container mx-auto text-center "> 
+  <div class="bg-gradient-to-r from-green-400 to-blue-500 px-6 py-10">
     <p class="text-8xl">Pick Your Plan</p>
   </div>
     
@@ -60,14 +61,101 @@
               </tr>
             </tbody>
           </table>
-        </div>      
+          
+        </div>   
+        <div>
+          <h3 class="text-xl font-bold text-white px-10">VOICE</h3>
+        </div>
+        <div class="bg-purple-400 rounded-lg mx-10">
+        
+        <table class="table-fixed text-xl text-center ">
+          <thead>
+            <tr class="bg-blue-200">
+              <th class="border border-purple-300">Provider</th>
+              <th class="border border-purple-300">Price</th>
+              <th class="border border-purple-300">Day</th>
+              <th class="border border-purple-300">Night</th>
+              <th class="w-1/4 border border-purple-300" >Activate</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="border border-purple-300">SLT broadband </td>
+              <td>Rs.650   </td>
+              <td class="border border-purple-300">8GB</td>
+              <td class="border border-purple-300">10GB</td>
+              <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 visit mobitel.lk</th>
+
+            </tr>
+            <tr class="bg-emerald-200">
+              <td class="border border-purple-300">Mobitel</td>
+              <td class="border border-purple-300">Rs.750</td>
+              <td class="border border-purple-300">13GB</td>
+              <td class="border border-purple-300">17GB</td>
+              <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 </th>
+            </tr>
+            <tr>
+              <td class="border border-purple-300">Dialog</td>
+              <td class="border border-purple-300">RS.600</td>
+              <td class="border border-purple-300">8GB</td>
+              <td class="border border-purple-300">13GB</td>
+              <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 </th>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+        <div class="flex flex-wrap mx-2 py-8">
+          <div class="w-full sm:w-1/2 nb-3 px2">
+              <div class="p-2 bg-gradient-to-r from-pink-600 h-full rounded-lg" >
+                  <h3 class="text-xl font-bold mb-3">Rs(1000-2000)</h3>
+             </div>
+            
+          </div>
+       </div>
+       <table class="table-fixed text-xl text-center ">
+        <thead>
+          <tr class="bg-blue-200">
+            <th class="border border-purple-300">Provider</th>
+            <th class="border border-purple-300">Price</th>
+            <th class="border border-purple-300">Day</th>
+            <th class="border border-purple-300">Night</th>
+            <th class="w-1/4 border border-purple-300" >Activate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="border border-purple-300">SLT broadband </td>
+            <td>Rs.650   </td>
+            <td class="border border-purple-300">8GB</td>
+            <td class="border border-purple-300">10GB</td>
+            <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 visit mobitel.lk</th>
+
+          </tr>
+          <tr class="bg-emerald-200">
+            <td class="border border-purple-300">Mobitel</td>
+            <td class="border border-purple-300">Rs.750</td>
+            <td class="border border-purple-300">13GB</td>
+            <td class="border border-purple-300">17GB</td>
+            <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 </th>
+          </tr>
+          <tr>
+            <td class="border border-purple-300">Dialog</td>
+            <td class="border border-purple-300">RS.600</td>
+            <td class="border border-purple-300">8GB</td>
+            <td class="border border-purple-300">13GB</td>
+            <th class="border border-purple-300">Dial #171# or 141 or sms INT D99 to 7678 </th>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    
+    
 </section>
 
 
 
-<section class="py-10">
-    <div class=" px-8">
+<section class="relative min-h-screen flex items-center">
+    <div class="container mx-auto text-center">
         <div class="flex flex-wrap mx-2">
             <div class="w-full sm:w-1/2 nb-3 px2">
                 <div class="p-2 bg-gradient-to-r from-pink-600 h-full rounded-lg" >
