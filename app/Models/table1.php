583608e4c7f8ba399bf5f9025b5cdb4aa7b310df
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-06-05 11:51:34 +0530

    database tables created

--- app/Models/table1.php
+++ app/Models/table1.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class 1500-2000t extends Model
+class table1 extends Model
 {
     use HasFactory;
 }
---
+++ database/migrations/2021_06_03_125610_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("provider", 120);
+            $table->integer('price');
+            $table->string("day", 60);
+            $table->string("night", 60);
+            $table->string("activate", 160);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
